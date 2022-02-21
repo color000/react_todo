952762faa7615680b35e2b36dd4394601c348dc8
color000 <backgroundcolorfff@gmail.com>
2022-02-21 16:18:39 +0900

    origin

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ const App = () => {
     const [todos, setTodos] = useState([
         {
             id: 1,
-            text: '리액트의 기초 알아보기',
+            text: '첫번째 리액트 포트폴리오 입니다.',
             checked: true,
         },
         {
             id: 2,
-            text: '컴포넌트 스타일링 해보기',
+            text: '체크박스와 버튼을 눌러보세요.',
             checked: true,
         },
         {
             id: 3,
-            text: '일정 관리 앱 만들어 보기',
+            text: '내용을 입력 후 엔터나 추가 버튼을 눌러보세요.',
             checked: false,
         }
     ]);
